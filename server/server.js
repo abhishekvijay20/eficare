d3042a00d4c54ffc4353948785392170854c5a8a
abhishekvijay20 <abhishek.jangid8@gmail.com>
2024-05-28 02:56:41 +0530

    pm2 install added in workflow 5

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const path = require('path');
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'static')));
 
 // File upload handling
 const storage = multer.memoryStorage();
